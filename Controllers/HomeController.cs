3842f19cf52473a50d23bf77008b8438322533e6
dsmv04 <dalton.munoz.v@hotmail.com>
2023-10-28 20:17:41 -0300

    Asignacion 4 Dia 4 Semana 7

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using SessionWorkshop.Models;
+using System;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult SetName(UserModel user)
+    {
+        // Guarda el nombre en la sesión
+        HttpContext.Session.SetString("UserName", user.UserName);
+        HttpContext.Session.SetInt32("Value", 22); // Inicializa el valor en la sesión
+        return RedirectToAction("Game");
+    }
+
+    [HttpPost]
+    public IActionResult UpdateValue(int increment)
+    {
+        // Obtén el valor actual de la sesión
+        int currentValue = HttpContext.Session.GetInt32("Value") ?? 0;
+
+        // Realiza la operación según el incremento
+        switch (increment)
+        {
+            case 1:
+                currentValue += 1;
+                break;
+            case -1:
+                currentValue -= 1;
+                break;
+            case 2:
+                currentValue *= 2;
+                break;
+            case 0:
+                // Generar un número aleatorio entre 1 y 10 (incluyendo 10)
+                Random random = new Random();
+                int randomIncrement = random.Next(1, 11);
+                currentValue += randomIncrement;
+                break;
+        }
+
+        // Guarda el nuevo valor en la sesión
+        HttpContext.Session.SetInt32("Value", currentValue);
+
+        return RedirectToAction("Game");
+    }
+
+    public IActionResult Game()
+    {
+        // Recupera el nombre de la sesión
+        string userName = HttpContext.Session.GetString("UserName");
+
+        // Recupera el valor de la sesión
+        int value = HttpContext.Session.GetInt32("Value") ?? 22;
+
+        // Crea una instancia del modelo para pasar datos a la vista
+        UserModel user = new UserModel { UserName = userName, Value = value };
+
+        return View("Game", user);
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,9 @@
+#pragma warning disable CS8618
+namespace SessionWorkshop.Models
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public int Value { get; set; }
+    }
+}
